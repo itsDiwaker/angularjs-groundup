3bdd86409f5743012da386e249d5121aa3a6004e
dtripati <dtripati@enspirecommerce.com>
2019-08-26 02:23:19 +0530

    Setup angular material theme

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,15 @@ angular
         '$mdThemingProvider', 
         function($mdThemingProvider) {
             
+            //configure ngMaterial library options
+            //configure 3rd oparty module configuration
             $mdThemingProvider
                 .theme('default')
                 .primaryPalette('teal')
                 .accentPalette('orange');
 
         }
-    ]);
+    ])
+    .run(function() {
+        console.log('App initialization complete!');
+    });
